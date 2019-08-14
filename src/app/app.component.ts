d58d37c5383671167199b158615824e43f9289b8
Leonardo Rodrigues <leonardo.ferreira@carambola.com.vc>
2019-08-14 14:13:27 -0300

    exemplo formulario reativo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
+import { Validacoes } from './Validacoes';
+import { Component, OnInit } from '@angular/core';
+
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '../../node_modules/@angular/forms';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ExemploReactiveForms';
+export class AppComponent implements OnInit {
+  // Aqui damos um nome para nosso formulário
+  // E ele precisa ser do tipo FormGroup
+  formularioDeUsuario: FormGroup;
+
+  // Via DI, nós obtemos o FormBuilder.
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.criarFormularioDeUsuario();
+  }
+
+  enviarDados() {
+    const dadosFormulario = this.formularioDeUsuario.value;
+
+    const usuario = new Usuario(
+      dadosFormulario.nome,
+      dadosFormulario.email,
+      dadosFormulario.cpf,
+      dadosFormulario.nascimento,
+      dadosFormulario.senha
+    );
+
+    alert(`O usuário ${usuario.nome} foi cadastrado com sucesso. \n Dados: ${JSON.stringify(usuario)}`);
+
+    this.formularioDeUsuario.reset();
+  }
+
+  criarFormularioDeUsuario() {
+    this.formularioDeUsuario = this.fb.group(
+      {
+        nome: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(100)
+          ])
+        ],
+        email: ['', Validators.compose([Validators.email])],
+        cpf: [
+          '',
+          Validators.compose([Validators.required, Validacoes.ValidaCpf])
+        ],
+        nascimento: [
+          '',
+          Validators.compose([Validators.required, Validacoes.MaiorQue18Anos])
+        ],
+        senha: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(12)
+          ])
+        ],
+        confirmarSenha: ['', Validators.compose([Validators.required])]
+      },
+      {
+        validator: Validacoes.SenhasCombinam
+      }
+    );
+  }
+
+  // Propriedades do formulário que vamos utilizar para obter os erros
+  get nome() {
+    return this.formularioDeUsuario.get('nome');
+  }
+
+  get email() {
+    return this.formularioDeUsuario.get('email');
+  }
+
+  get cpf() {
+    return this.formularioDeUsuario.get('cpf');
+  }
+
+  get nascimento() {
+    return this.formularioDeUsuario.get('nascimento');
+  }
+
+  get senha() {
+    return this.formularioDeUsuario.get('senha');
+  }
+
+  get confirmarSenha() {
+    return this.formularioDeUsuario.get('confirmarSenha');
+  }
 }
